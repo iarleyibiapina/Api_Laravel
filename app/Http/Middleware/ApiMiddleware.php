65fbb4933e79c86ecd4b4138339030b5f43f98f9
Iarley Ibiapina Barbosa <iarley.ibiapina@opovodigital.com>
2024-02-02 18:10:43 -0300

    Feat: Cria Middleware JWT para Api

---
+++ app/Http/Middleware/ApiMiddleware.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
+
+class ApiMiddleware extends BaseMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch (\Exception $e) {
+            if ($e instanceof TokenInvalidException) {
+                return response()->json([
+                    'status' => 'token is invalid'
+                ]);
+            } else if ($e instanceof TokenExpiredException) {
+                return response()->json([
+                    'status' => 'token is expired'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => 'Authorization token not found'
+                ]);
+            }
+        }
+
+        return $next($request);
+    }
+}
